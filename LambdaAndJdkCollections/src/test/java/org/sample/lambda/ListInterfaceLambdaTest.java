43b303b1eb9cca63111ca01a888cf0e10ec9fd3d
ericfan <fanlei77@hotmail.com>
2017-05-03 17:26:53 +0800

    use slf4j instead

--- LambdaAndJdkCollections/src/test/java/org/sample/lambda/ListInterfaceLambdaTest.java
+++ LambdaAndJdkCollections/src/test/java/org/sample/lambda/ListInterfaceLambdaTest.java
@@ -3,14 +3,14 @@ package org.sample.lambda;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
-public class ListInterfaceLambda {
+public class ListInterfaceLambdaTest {
 
-	Logger logger = Logger.getAnonymousLogger();
+	private static final Logger logger = LoggerFactory.getLogger(ListInterfaceLambdaTest.class);
 
 	private List<String> sampleStringList = new ArrayList<>(
 			Arrays.asList("You", "must", "be", "a", "professional", "guy"));
@@ -19,7 +19,7 @@ public class ListInterfaceLambda {
 	public void loopListForJdk7() {
 		for (String str : sampleStringList) {
 			if (str.length() < 4)
-				logger.log(Level.INFO, str);
+				logger.info(str);
 		}
 	}
 
@@ -27,7 +27,7 @@ public class ListInterfaceLambda {
 	public void loopListForJdk8() {
 		sampleStringList.forEach(str -> {
 			if (str.length() > 4)
-				logger.log(Level.INFO, str);
+				logger.info(str);
 		});
 	}
 
