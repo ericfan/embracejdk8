a5afb46190bcb8eb3996e8f055116a6ea5e8dbe3
ericfan <fanlei77@hotmail.com>
2017-05-04 14:57:44 +0800

    Add sort lambda test

--- LambdaAndJdkCollections/src/test/java/org/sample/lambda/AllTests.java
+++ LambdaAndJdkCollections/src/test/java/org/sample/lambda/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ LoopElementTest.class, RemoveElementTest.class, ReplaceElementTest.class })
+@SuiteClasses({ LoopElementTest.class, RemoveElementTest.class, ReplaceElementTest.class, SortElementTest.class })
 public class AllTests {
 
 }
---
+++ LambdaAndJdkCollections/src/test/java/org/sample/lambda/SortElementTest.java
@@ -0,0 +1,45 @@
+package org.sample.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SortElementTest {
+
+	private List<String> sampleStringList;
+
+	private static final Logger logger = LoggerFactory.getLogger(LoopElementTest.class);
+
+	@Before
+	public void beforeTest() {
+		sampleStringList = new ArrayList<>(Arrays.asList("You", "must", "be", "a", "professional", "guy"));
+	}
+
+	/**
+	 * Collections sort method will sort the list element uppercase firstly.
+	 */
+	@Test
+	public void sortElementForJdk7() {
+		Collections.sort(sampleStringList);
+		Assert.assertEquals(6, sampleStringList.size());
+		sampleStringList.forEach(str -> {logger.info(str);});
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void sortElementForJdk8() {
+		sampleStringList.sort((str1, str2) -> str1.toLowerCase().compareTo(str2.toLowerCase()));
+		Assert.assertEquals(6, sampleStringList.size());
+		sampleStringList.forEach(str -> {logger.info(str);});
+	}
+	
+}
