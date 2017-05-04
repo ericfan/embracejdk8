bad9a0b15811db55f5513428ac7509bd9e325557
ericfan <fanlei77@hotmail.com>
2017-05-04 12:09:04 +0800

    add replace lambda sample

--- LambdaAndJdkCollections/src/test/java/org/sample/lambda/AllTests.java
+++ LambdaAndJdkCollections/src/test/java/org/sample/lambda/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ LoopElementTest.class, RemoveElementTest.class })
+@SuiteClasses({ LoopElementTest.class, RemoveElementTest.class, ReplaceElementTest.class })
 public class AllTests {
 
 }
---
+++ LambdaAndJdkCollections/src/test/java/org/sample/lambda/ReplaceElementTest.java
@@ -0,0 +1,50 @@
+package org.sample.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Transformer;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReplaceElementTest {
+
+	private List<String> sampleStringList;
+
+	@Before
+	public void beforeTest() {
+		sampleStringList = new ArrayList<>(Arrays.asList("You", "must", "be", "a", "professional", "guy"));
+	}
+
+	@Test
+	public void replaceElementForApacheCommons() {
+		CollectionUtils.transform(sampleStringList, new Transformer<String, String>() {
+			@Override
+			public String transform(String input) {
+				if (input.length() > 3)
+					return input.toUpperCase();
+				return input;
+			}
+
+		});
+		Assert.assertEquals(6, sampleStringList.size());
+		Assert.assertEquals("MUST", sampleStringList.get(1));
+	}
+	
+	@Test
+	public void replaceElementForJdk8(){
+		sampleStringList.replaceAll(str -> {
+			if(str.length()>3)
+				return str.toUpperCase();
+			return str;
+		});
+		Assert.assertEquals(6, sampleStringList.size());
+		Assert.assertEquals("MUST", sampleStringList.get(1));
+	}
+	
+	
+
+}
