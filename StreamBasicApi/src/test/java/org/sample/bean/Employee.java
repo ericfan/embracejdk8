8162b30ef77bb4c03cd60442741e22cf252e6aae
ericfan <fanlei77@hotmail.com>
2017-05-19 17:04:53 +0800

    add stream map sample

---
+++ StreamBasicApi/src/test/java/org/sample/bean/Employee.java
@@ -0,0 +1,18 @@
+package org.sample.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+	private long anum;
+	
+	private String fullName;
+	
+	
+	
+}
